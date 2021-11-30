e713dd6b11bd320ab4d340e4fbbce4d09d9b8ae9
Klay Clarke <klayaclarke@gmail.com>
2021-11-30 16:32:22 -0500

    use find_flight_info method to find info on cheapest flight for each entry (city) in google sheet doc - if flight price is greater than our lowest price willing to pay (listed in google sheet), retrieve no data for said destination (means that all flights into that destination are more expensive that we are willing to pay)

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
 
 fs = FlightSearch()
+fd = FlightData()
 dm = DataManager()
+
 city_data = dm.read_rows()
 
+# if flight price is less that our lowest price listed in google sheets, print price in console
 for city in city_data:
-    if city['iataCode'] == '':
-        city_code = fs.find_city_code(city['city'])
-        dm.update_iata_codes(city_code, city['id'])
+    cheapest_flight_info = (fd.find_flight_info(city_code=city['iataCode'],max_price=city['lowestPrice']))
+    try:
+        city_name = (cheapest_flight_info['data'][0]['cityTo'])
+        flight_price = (cheapest_flight_info['data'][0]['price'])
+        link_to_deal = (cheapest_flight_info['data'][0]['deep_link'])
+        print(f'{city_name}: £{flight_price}\nLink: {link_to_deal} ')
+    except IndexError:
+        pass
