1af3cfb70641b9b312e78df915a95ec98d0b78f6
Klay Clarke <klayaclarke@gmail.com>
2021-11-30 09:19:58 -0500

    use tequila flight search API to retrieve information on Paris airports and flights

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,17 @@
+import os
+
+TEQUILA_API_KEY = os.environ.get('TEQUILA_API_KEY')
+
+tequila_header = {
+    'apikey': TEQUILA_API_KEY,
+}
+
+tequila_params = {
+    'term': 'Paris'
+}
+
+tequila_endpoint = 'https://tequila-api.kiwi.com/locations/query'
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     pass
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes
 # to achieve the program requirements.
+import requests
+
+import flight_search as fs
+
+tequila_response = requests.get(url=fs.tequila_endpoint, params=fs.tequila_params, headers=fs.tequila_header)
+tequila_response.raise_for_status()
+flight_search_data = tequila_response.json()
+print(flight_search_data)
+
