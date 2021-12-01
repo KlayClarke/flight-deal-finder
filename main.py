b176ddec0481ad06bcff0fd737f07278a720b41b
Klay Clarke <klayaclarke@gmail.com>
2021-11-30 23:41:14 -0500

    send email to users if available flight prices are less than lowest price listed in google sheet

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ nm = NotificationManager()
 
 city_data = dm.read_rows()
 
-
-# if price is less than our lowest price listed in google sheets, sens sms
+# if price is less than our lowest price listed in google sheets, sens email to users
 for city in city_data:
     cheapest_flight_info = fd.find_flight_info(city_code=city['iataCode'], max_price=city['lowestPrice'])
     try:
@@ -22,24 +21,8 @@ for city in city_data:
     except IndexError:
         pass
     else:
-        sms_text = f'Low price alert! Only ${flight_price} to fly from Boston ' \
+        email_text = f'Low price alert! Only ${flight_price} to fly from Boston ' \
                    f'and spend {nights_in_destination} days in {city_name}\n' \
                    f'Link: {link_to_deal}'
-        nm.send_sms(sms_text=sms_text)
+        nm.send_email(email_text=email_text)
 
-# # populate iata codes for each entry in google sheet if they are empty
-# for city in city_data:
-#     if city['iataCode'] == '':
-#         print(dm.update_iata_codes(city_code=fs.find_city_code(city['city']), id=city['id']))
-
-# # if flight price is less than our lowest price listed in google sheets, print price in console
-# for city in city_data:
-#     cheapest_flight_info = (fd.find_flight_info(city_code=city['iataCode'],max_price=city['lowestPrice']))
-#     try:
-#         city_name = (cheapest_flight_info['data'][0]['cityTo'])
-#         nights_in_destination = (cheapest_flight_info['data'][0]['nightsInDest'])
-#         flight_price = (cheapest_flight_info['data'][0]['price'])
-#         link_to_deal = (cheapest_flight_info['data'][0]['deep_link'])
-#         print(f'${flight_price}\n{nights_in_destination} nights in {city_name}\nLink: {link_to_deal} ')
-#     except IndexError:
-#         pass
